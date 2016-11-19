bc8e206a3f4f3dd744d07aac40460eb64b8834a5
Tomasz <konopczynski@volumegraphics.com>
2016-11-19 13:53:12 +0100

    change printing from 'a' to 'alpha'

--- scripts/LearnDictionary/LearnDictionary.py
+++ scripts/LearnDictionary/LearnDictionary.py
@@ -32,7 +32,7 @@ def learn_dictionary_mini(patches, n_c=512, a=1, n_i=800, n_j=3, b_s=3, es=5, fi
     """
     dic = MiniBatchDictionaryLearning(n_components=n_c, alpha=a, n_iter=n_i,
                                       n_jobs=n_j, batch_size=b_s, fit_algorithm=fit_algorithm)
-    print ("Start learning dictionary_mini: n_c: "+str(n_c)+", a: "+str(a)+", n_i: " +
+    print ("Start learning dictionary_mini: n_c: "+str(n_c)+", alpha: "+str(a)+", n_i: " +
            str(n_i)+", n_j: "+str(n_j)+", es: "+str(es)+", b_s: "+str(b_s))
     v1 = dic.fit(patches).components_
     d1 = v1.reshape(n_c, es, es, es)  # e.g. 512x5x5x5
@@ -42,7 +42,7 @@ def learn_dictionary_mini(patches, n_c=512, a=1, n_i=800, n_j=3, b_s=3, es=5, fi
 def learn_dictionary(patches, n_c=512, a=1, n_i=100, n_j=3, es=5, fit_algorithm='lars'):
     dic = DictionaryLearning(n_components=n_c, alpha=a, max_iter=n_i,
                              n_jobs=n_j, fit_algorithm=fit_algorithm)
-    print ("Start learning dictionary: n_c: "+str(n_c)+", a: "+str(a)+", n_i: " +
+    print ("Start learning dictionary: n_c: "+str(n_c)+", alpha: "+str(a)+", n_i: " +
            str(n_i)+", es: "+str(es)+", n_j: "+str(n_j))
     v2 = dic.fit(patches).components_
     d2 = v2.reshape(n_c, es, es, es)  # e.g. 512x5x5x5
